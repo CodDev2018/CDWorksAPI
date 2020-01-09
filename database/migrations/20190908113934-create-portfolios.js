53f799d50d6a2dafca720400fe5dacddb44436e6
Paulo Bezerra <bezerra.paulo@gmail.com>
2020-01-09 09:26:56 -0400

    Update 20190908113934-create-portfolios.js

--- database/migrations/20190908113934-create-portfolios.js
+++ database/migrations/20190908113934-create-portfolios.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Portfolios', {
+    return queryInterface.createTable('portfolios', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -40,6 +40,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Portfolios');
+    return queryInterface.dropTable('portfolios');
   }
 };
